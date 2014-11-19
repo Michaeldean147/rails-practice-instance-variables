22a6d8d1043fd5d705028f89ad701816b8e3ced7
Michaeldean147 <michaeldean147@yahoo.com>
2014-11-19 13:31:56 -0800

    exercise commit

--- app/controllers/cupcakes_controller.rb
+++ app/controllers/cupcakes_controller.rb
@@ -1,9 +1,15 @@
 class CupcakesController < ApplicationController
-  def vanilla
+  attr_reader :sizes, :toppings, :frostings, :ingredients
+
+  def sizes
+    @sizes = ["small", "medium", "large"]
+  end
+
+  def toppings
     @toppings = ['sprinkles', 'frosting', 'gummy worms']
   end
 
-  def chocolate
+  def frostings
     @frostings = {
       vanilla: '2 dollops',
       chocolate: 'thin spread',
@@ -11,11 +17,43 @@ class CupcakesController < ApplicationController
     }
   end
 
-  def maple_bacon
+  def ingredients
+    @ingredients = {
+      flour: '1 1/2 cup',
+      sugar: '1 cup',
+      soda: "1 tsp",
+      salt: "1 tsp",
+      oil: "1/2 cup",
+      water: "1 cup",
+      vinegar: "1 tsp"
+    }
+  end
 
+  def vanilla
+    sizes
+    toppings
+    frostings
+    ingredients
+  end
+
+  def chocolate
+    sizes
+    toppings
+    frostings
+    ingredients
+  end
+
+  def maple_bacon
+    sizes
+    toppings
+    frostings
+    ingredients
   end
 
   def red_velvet
-
+    sizes
+    toppings
+    frostings
+    ingredients
   end
 end
